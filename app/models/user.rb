4062befb27361c53f2f7bfe8512d2c990e5ec504
Patricio <pat.venegas.l@gmail.com>
2018-02-20 15:42:59 -0300

    primer commmit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+	has_many: playlists
+	validates :name, presence: true
+	validates :name, uniqueness: true
+	validates :email, presence: true
+	validates :email, uniqueness: true
+
+end
